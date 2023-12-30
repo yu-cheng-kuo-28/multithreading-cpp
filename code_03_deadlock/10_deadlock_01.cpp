6c1c1828fb5f560fd0612e7c41dce376ccd9ca81
yu-cheng-kuo-28 <yc.kuo.28@gmail.com>
2023-12-31 01:58:45 +0800

    Revsie code a bit

--- code_03_deadlock/10_deadlock_01.cpp
+++ code_03_deadlock/10_deadlock_01.cpp
@@ -2,21 +2,21 @@
 #include <thread>
 #include <mutex>
 
-std::mutex mutex1;
-std::mutex mutex2;
+std::mutex resource1;
+std::mutex resource2;
 
 void threadFunction1() {
-    std::lock_guard<std::mutex> lock1(mutex1);
+    std::lock_guard<std::mutex> lock1(resource1);
     std::this_thread::sleep_for(std::chrono::milliseconds(100)); // Simulating work
-    std::lock_guard<std::mutex> lock2(mutex2); // This will wait for mutex2
+    std::lock_guard<std::mutex> lock2(resource2); // This will wait for resource2
 
     std::cout << "Thread 1 has both mutexes" << std::endl;
 }
 
 void threadFunction2() {
-    std::lock_guard<std::mutex> lock2(mutex2);
+    std::lock_guard<std::mutex> lock2(resource2);
     std::this_thread::sleep_for(std::chrono::milliseconds(100)); // Simulating work
-    std::lock_guard<std::mutex> lock1(mutex1); // This will wait for mutex1
+    std::lock_guard<std::mutex> lock1(resource1); // This will wait for resource1
 
     std::cout << "Thread 2 has both mutexes" << std::endl;
 }
--- code_03_deadlock/11_deadlock_02.cpp
+++ code_03_deadlock/11_deadlock_02.cpp
@@ -3,21 +3,23 @@
 #include <mutex>
 #include <utility>
 
-std::mutex mutex1;
-std::mutex mutex2;
+std::mutex resource1;
+std::mutex resource2;
 
 void threadFunction1() {
-    std::lock(mutex1, mutex2); // Lock both mutexes without deadlock
-    std::lock_guard<std::mutex> lock1(mutex1, std::adopt_lock);
-    std::lock_guard<std::mutex> lock2(mutex2, std::adopt_lock);
+    std::lock(resource1, resource2); // Lock both mutexes without deadlock
+    std::lock_guard<std::mutex> lock1(resource1, std::adopt_lock);
+    std::this_thread::sleep_for(std::chrono::milliseconds(100)); // Simulating work
+    std::lock_guard<std::mutex> lock2(resource2, std::adopt_lock);
 
     std::cout << "Thread 1 has both mutexes" << std::endl;
 }
 
 void threadFunction2() {
-    std::lock(mutex1, mutex2); // Lock both mutexes without deadlock
-    std::lock_guard<std::mutex> lock1(mutex1, std::adopt_lock);
-    std::lock_guard<std::mutex> lock2(mutex2, std::adopt_lock);
+    std::lock(resource1, resource2); // Lock both mutexes without deadlock
+    std::lock_guard<std::mutex> lock2(resource2, std::adopt_lock);
+    std::this_thread::sleep_for(std::chrono::milliseconds(100)); // Simulating work
+    std::lock_guard<std::mutex> lock1(resource1, std::adopt_lock);
 
     std::cout << "Thread 2 has both mutexes" << std::endl;
 }
